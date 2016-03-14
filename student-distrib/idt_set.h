173119fd92b064765ef6a3f22cf02e06ff021a45
timwang2 <timwang2@illinois.edu>
2016-03-14 01:31:07 -0500

    did some edits to make it usable

--- student-distrib/idt_set.h
+++ student-distrib/idt_set.h
@@ -7,13 +7,17 @@
 #define QUAD_SIZE 8
 
 
-extern void set_int_gate(unsigned long n, void(*addr)());
+extern void set_int_gate(unsigned long n, unsigned long addr);
 
-extern void set_sys_gate(unsigned long n, void(*addr)());
 
-extern void set_sys_int_gate(unsigned long n, void(*addr)());
+extern void set_sys_gate(unsigned long n, unsigned long addr);
 
-extern void set_trap_gate(unsigned long n, void(*addr)());
+
+extern void set_sys_int_gate(unsigned long n, unsigned long addr);
+
+
+extern void set_trap_gate(unsigned long n, unsigned long addr);
+
 
 extern void set_task_gate(unsigned long n, unsigned long gdt);
 #endif //_IDT_SET_FUNC_
