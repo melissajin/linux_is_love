c157d63fdddcb642bbd0391110733ad482b24925
timwang2 <timwang2@illinois.edu>
2016-03-14 01:31:07 -0500

    did some edits to make it usable

--- student-distrib/idt_set.c
+++ student-distrib/idt_set.c
@@ -1,62 +1,58 @@
 #include "idt_set.h"
 
 
-void set_int_gate(unsigned long n, void(*addr)()){
+void set_int_gate(unsigned long n, unsigned long addr){
 	idt_desc_t newidt;
-	newidt.offset_15_00 = 0xFF & (unsigned int)addr;
+	SET_IDT_ENTRY(newidt, addr)
 	newidt.seg_selector = KERNEL_CS;
 	newidt.reserved2 = 1;
 	newidt.reserved1 = 1;
 	newidt.size = 1;
 	newidt.dpl = 0;
-	newidt.offset_31_16 = (0xFF00 & (unsigned int)addr)>>16;
 	
 	newidt.present = 1;
 	idt[n] =  newidt;
 }
 
 
-void set_sys_gate(unsigned long n, void(*addr)()){
+void set_sys_gate(unsigned long n, unsigned long addr){
 	idt_desc_t newidt;
-	newidt.offset_15_00 = 0xFF & (unsigned int)addr;
+	SET_IDT_ENTRY(newidt, addr)
 	newidt.seg_selector = KERNEL_CS;
 	newidt.reserved3 =1;
 	newidt.reserved2 =1;
 	newidt.reserved1 = 1;
 	newidt.size = 1;
 	newidt.dpl = 3;
-	newidt.offset_31_16 = (0xFF00 & (unsigned int)addr)>>16;
 	
 	newidt.present = 1;
 	idt[n] =  newidt;
 }
 
 
-void set_sys_int_gate(unsigned long n, void(*addr)()){
+void set_sys_int_gate(unsigned long n, unsigned long addr){
 	idt_desc_t newidt;
-	newidt.offset_15_00 = 0xFF & (unsigned int)addr;
+	SET_IDT_ENTRY(newidt, addr)
 	newidt.seg_selector = KERNEL_CS;
 	newidt.reserved2 =1;
 	newidt.reserved1 = 1;
 	newidt.size = 1;
 	newidt.dpl = 3;
-	newidt.offset_31_16 = (0xFF00 &  (unsigned int)addr)>>16;
 	
 	newidt.present = 1;
 	idt[n] =  newidt;
 }
 
 
-void set_trap_gate(unsigned long n, void(*addr)()){
+void set_trap_gate(unsigned long n, unsigned long addr){
 	idt_desc_t newidt;
-	newidt.offset_15_00 = 0xFF & (unsigned int)addr;
+	SET_IDT_ENTRY(newidt, addr)
 	newidt.seg_selector = KERNEL_CS;
 	newidt.reserved3 =1;
 	newidt.reserved2 =1;
 	newidt.reserved1 = 1;
 	newidt.size = 1;
 	newidt.dpl = 0;
-	newidt.offset_31_16 = (0xFF00 & (unsigned int)addr)>>16;
 	
 	
 	newidt.present = 1;
--- student-distrib/isr.c
+++ student-distrib/isr.c
@@ -212,25 +212,25 @@ void isr18()
 
 /* Set first 32 entries in IDT to Intel's defined exceptions */
 void isrs_install(){
-	set_trap_gate(0,  isr0);
-	set_trap_gate(1,  isr1);
-	set_trap_gate(2,  isr2);
-	set_trap_gate(3,  isr3);
-	set_trap_gate(4,  isr4);
-	set_trap_gate(5,  isr5);
-	set_trap_gate(6,  isr6);
-	set_trap_gate(7,  isr7);
-	set_trap_gate(8,  isr8);
-	set_trap_gate(9,  isr9);
-	set_trap_gate(10,  isr10);
-	set_trap_gate(11,  isr11);
-	set_trap_gate(12,  isr12);
-	set_trap_gate(13,  isr13);
-	set_trap_gate(14,  isr14);
-	set_trap_gate(15,  isr15);
-	set_trap_gate(16,  isr16);
-	set_trap_gate(17,  isr17);
-	set_trap_gate(18,  isr18);
+	set_trap_gate(0,  (unsigned long)isr0);
+	set_trap_gate(1,  (unsigned long)isr1);
+	set_trap_gate(2,  (unsigned long)isr2);
+	set_trap_gate(3,  (unsigned long)isr3);
+	set_trap_gate(4,  (unsigned long)isr4);
+	set_trap_gate(5,  (unsigned long)isr5);
+	set_trap_gate(6,  (unsigned long)isr6);
+	set_trap_gate(7,  (unsigned long)isr7);
+	set_trap_gate(8,  (unsigned long)isr8);
+	set_trap_gate(9,  (unsigned long)isr9);
+	set_trap_gate(10,  (unsigned long)isr10);
+	set_trap_gate(11,  (unsigned long)isr11);
+	set_trap_gate(12,  (unsigned long)isr12);
+	set_trap_gate(13,  (unsigned long)isr13);
+	set_trap_gate(14,  (unsigned long)isr14);
+	set_trap_gate(15,  (unsigned long)isr15);
+	set_trap_gate(16,  (unsigned long)isr16);
+	set_trap_gate(17,  (unsigned long)isr17);
+	set_trap_gate(18,  (unsigned long)isr18);
 }
 
 
