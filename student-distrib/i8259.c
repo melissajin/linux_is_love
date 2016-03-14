8ffe83fb716513952ab35ec46c1d827123ce9f4b
timwang2 <timwang2@illinois.edu>
2016-03-14 16:24:15 -0500

    LAST COMMENTS I SWEAR

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -5,6 +5,10 @@
 #include "i8259.h"
 #include "lib.h"
 
+#define SLAVE_IRQ 	2
+#define IRQ_ALL_MASK	0xFF
+#define IRQ_PER_PIC	8
+
 /* Interrupt masks to determine which interrupts
  * are enabled and disabled */
 uint8_t master_mask; /* IRQs 0-7 */
@@ -15,9 +19,9 @@ void
 i8259_init(void)
 {
 
-	/* Save masks */
-	master_mask = 0xff; 
-	slave_mask = 0xff;
+	/* Set masks */
+	master_mask = IRQ_ALL_MASK; 
+	slave_mask = IRQ_ALL_MASK;
 
 	/* ICW1: Start PIC initization sequence, no ICW4, 2 PICs, edge triggered */
 	outb(ICW1, MASTER_8259_PORT);
@@ -48,18 +52,18 @@ enable_irq(uint32_t irq_num)
 	uint16_t port;
 	uint8_t value;
 
-	if(irq_num < 8){ 		//if interrupt occurs on master pic
+	if(irq_num < IRQ_PER_PIC){ 		//if interrupt occurs on master pic
 		port = MASTER_8259_PORT_DATA;
 		value = inb(port) & ~(1 << irq_num);
 		outb(value, port);
 	}
 	else{				//if interrupt occurs on slave pic
 		port = SLAVE_8259_PORT_DATA;
-		irq_num -= 8;
+		irq_num -= IRQ_PER_PIC;
 		value = inb(port) & ~(1 << irq_num); 
 		outb(value, port); 	
 		port = MASTER_8259_PORT_DATA;	//mask slave pic on master pic
-		value = inb(port) & ~(1 << 2);
+		value = inb(port) & ~(1 << SLAVE_IRQ);
 		outb(value, port);
 	}
 	
@@ -74,18 +78,18 @@ disable_irq(uint32_t irq_num)
 	uint16_t port;
 	uint8_t value;
 
-	if(irq_num < 8){
+	if(irq_num < IRQ_PER_PIC){
 		port = MASTER_8259_PORT_DATA;
 		value = inb(port) | (1 << irq_num); //mask the master pic
 		outb(value, port);
 	}
 	else{
 		port = SLAVE_8259_PORT_DATA; //mask the slave pic
-		irq_num -= 8;
+		irq_num -= IRQ_PER_PIC;
 		value = inb(port) | (1 << irq_num);
 		outb(value, port);
 		port = MASTER_8259_PORT_DATA; //mask the master pic
-		value = inb(port) | (1 << 2);
+		value = inb(port) | (1 << SLAVE_IRQ);
 		outb(value, port);
 	}
 }
@@ -95,13 +99,13 @@ disable_irq(uint32_t irq_num)
 void
 send_eoi(uint32_t irq_num)
 {
-	if(irq_num < 8){
+	if(irq_num < IRQ_PER_PIC){
 		outb((EOI | irq_num), MASTER_8259_PORT); /* Send EOI to master pic */
 	}
 	else{
-		irq_num -= 8;
+		irq_num -= IRQ_PER_PIC;
 		outb((EOI | irq_num), SLAVE_8259_PORT); /* Send EOI to slave pic */
-		outb((EOI | 2), MASTER_8259_PORT); /* Send EOI to master pic */
+		outb((EOI | SLAVE_IRQ), MASTER_8259_PORT); /* Send EOI to master pic */
 	}
 	
 }
