da4e268bb408060d59abfef07a07b77da6ca07e6
Joshua Zeid <jrzeid23@gmail.com>
2016-03-22 20:58:16 -0500

    Add support for changing RTC rate

--- student-distrib/devices/rtc.c
+++ student-distrib/devices/rtc.c
@@ -7,36 +7,32 @@
 #include "../idt_set.h"
 #include "../lib.h"
 
-#define NMI_DISABLE 0x80
-#define REG_A	    0xA
-#define REG_B	    0xB
-#define REG_C	    0xC
+#define NMI_DISABLE  0x80
+#define REG_A	     0xA
+#define REG_B	     0xB
+#define REG_C	     0xC
 
-
+#define RATE_DEFAULT 0xF
+#define RATE_MAX     1024
+#define RTC_ABS_MAX  32768
 
 extern void rtc_isr();
 
 void rtc_init(){
 
-	char curr;
-	char rate;
-  /* Populate IDT entry for rtc */
-  set_int_gate(0x28, (unsigned long) rtc_isr);
+    char curr;
+    /* Populate IDT entry for rtc */
+    set_int_gate(0x28, (unsigned long) rtc_isr);
 
-  /* Turn on RTC interrupts */
+    /* Turn on RTC interrupts */
+    cli();  /* don't interrupt so rtc is not let in undefined state */
 	outb(NMI_DISABLE | REG_B, RTC_REG_PORT);	// select register B and disable NMI
-	curr = inb(RW_CMOS_PORT);			// read the current value of register B
+    curr = inb(RW_CMOS_PORT);			// read the current value of register B
 	outb(NMI_DISABLE | REG_B, RTC_REG_PORT);	// reset the register to B again
 	outb(curr | 0x40, RW_CMOS_PORT); 		// turn on bit 6 of register B
+    sti();
 
-  /* Change the rate of interrupts */
-  rate = 0x0f; // Rate has to be between 2 and 15
-  outb(NMI_DISABLE | REG_A, RTC_REG_PORT);
-  curr = inb(RW_CMOS_PORT);
-  outb(NMI_DISABLE | REG_A, RTC_REG_PORT);
-  outb(RW_CMOS_PORT, (curr & 0xF0) | rate); 		//0xF0 a mask to take top 4 bits 
-
-  enable_irq(RTC_IRQ_NUM);
+    enable_irq(RTC_IRQ_NUM);
 }
 
 void rtc_handler_main(){
@@ -47,4 +43,44 @@ void rtc_handler_main(){
   inb(RW_CMOS_PORT);
 
   send_eoi(RTC_IRQ_NUM);
+}
+
+/* open rtc */
+unsigned long rtc_open() {
+
+}
+
+/* wait until next rtc interrupt */
+unsigned long rtc_read() {
+
+}
+
+/* change rtc frequency */
+unsigned long rtc_write(unsigned long rate) {
+    unsigned long ratefactor;
+    char curr, rs = 0;  /* rate select */
+    /* make sure rate is in range */
+    if(rate == 0 || rate > RATE_MAX)
+        return -1;
+    /* sanity check for power of 2 */
+    /* from http://www.exploringbinary.com/ten-ways-to-check-if-an-integer-is-a-power-of-two-in-c/ */
+    if((x != 0) && !(x & (x - 1)))
+        return -1;
+
+    ratefactor = RTC_ABS_MAX / rate;
+    while(ratefactor >>= 1) {
+        rs++;
+    }
+
+    cli();
+    outb(NMI_DISABLE | REG_A, RTC_REG_PORT);
+    curr = inb(RW_CMOS_PORT);
+    outb(NMI_DISABLE | REG_A, RTC_REG_PORT);
+    outb(RW_CMOS_PORT, (curr & 0xF0) | rs);       //0xF0 a mask to take top 4 bits
+    sti();
+}
+
+/* close rtc */
+unsigned long rtc_close() {
+
 }
--- student-distrib/devices/rtc.h
+++ student-distrib/devices/rtc.h
@@ -11,9 +11,22 @@
                               // and to disable NMI
 #define RW_CMOS_PORT     0x71 // Port used to read/write from from CMOS
 #define RTC_IRQ_NUM  0x08
+
 //initializes rtc
 void rtc_init();
 //rtc interrupt
 void rtc_handler_main();
+
+/* open rtc */
+unsigned long rtc_open();
+
+/* wait until next rtc interrupt */
+unsigned long rtc_read();
+
+/* change rtc frequency */
+unsigned long rtc_write(unsigned long rate);
+
+/* close rtc */
+unsigned long rtc_close();
 
 #endif
