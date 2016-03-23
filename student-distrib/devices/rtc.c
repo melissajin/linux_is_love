2fd7bba0bc9d196bf505c078045c1a7360769e9a
Joshua Zeid <jrzeid23@gmail.com>
2016-03-23 00:25:26 -0500

    Fixed bugs in rtc driver functions.
    More work to do once IRQs can be dynamically installed. Also, consider adding more robust support for multiple opens/closes

--- student-distrib/devices/rtc.c
+++ student-distrib/devices/rtc.c
@@ -21,20 +21,8 @@ extern void rtc_isr();
 static int reading, open = 0;
 
 void rtc_init(){
-
-    char curr;
     /* Populate IDT entry for rtc */
-    set_int_gate(0x28, (unsigned long) rtc_isr);
-
-    /* Turn on RTC interrupts */
-    cli();  /* don't interrupt so rtc is not let in undefined state */
-	outb(NMI_DISABLE | REG_B, RTC_REG_PORT);	// select register B and disable NMI
-    curr = inb(RW_CMOS_PORT);			// read the current value of register B
-	outb(NMI_DISABLE | REG_B, RTC_REG_PORT);	// reset the register to B again
-	outb(curr | 0x40, RW_CMOS_PORT); 		// turn on bit 6 of register B
-    sti();
-
-    reading = 1;
+    set_int_gate(0x28, (unsigned long) rtc_isr);    
 }
 
 void rtc_handler_main(){
@@ -51,15 +39,24 @@ void rtc_handler_main(){
 
 /* open rtc */
 unsigned long rtc_open() {
+    char curr;
     if(!open) {
-        rtc_init();
+        /* Turn on RTC interrupts */
+        cli();  /* don't interrupt so rtc is not let in undefined state */
+        outb(NMI_DISABLE | REG_B, RTC_REG_PORT);    // select register B and disable NMI
+        curr = inb(RW_CMOS_PORT);           // read the current value of register B
+        outb(NMI_DISABLE | REG_B, RTC_REG_PORT);    // reset the register to B again
+        outb(curr | 0x40, RW_CMOS_PORT);        // turn on bit 6 of register B
+        sti();
         enable_irq(RTC_IRQ_NUM);
         open = 1;
     }
+    return 0;
 }
 
 /* wait until next rtc interrupt */
 unsigned long rtc_read() {
+    reading = 1;
     while(reading);  /* spin until an interrupt happens */
     reading = 1;
     return 0;
@@ -73,25 +70,39 @@ unsigned long rtc_write(unsigned long rate) {
     if(rate == 0 || rate > RATE_MAX)
         return -1;
     /* sanity check for power of 2 */
-    /* from http://www.exploringbinary.com/ten-ways-to-check-if-an-integer-is-a-power-of-two-in-c/ */
-    if((x != 0) && !(x & (x - 1)))
+    /* from http://www.exploringbinary.com/ten-ways-to-check-if-an-integer-is-a-power-of-two-in-c/ */ 
+    if(rate & (rate - 1))  /* will only be 0 if rate is a power of 2 */
         return -1;
 
     ratefactor = RTC_ABS_MAX / rate;
     while(ratefactor >>= 1) {
         rs++;
     }
+    rs++;
 
     cli();
     outb(NMI_DISABLE | REG_A, RTC_REG_PORT);
     curr = inb(RW_CMOS_PORT);
     outb(NMI_DISABLE | REG_A, RTC_REG_PORT);
-    outb(RW_CMOS_PORT, (curr & 0xF0) | rs);       //0xF0 a mask to take top 4 bits
+    outb((curr & 0xF0) | rs, RW_CMOS_PORT);       //0xF0 a mask to take top 4 bits
     sti();
+
+    return 0;
 }
 
 /* close rtc */
 unsigned long rtc_close() {
+    char curr;
     disable_irq(RTC_IRQ_NUM);
     open = 0;
+
+    /* turn off RTC interrupts */
+    cli();
+    outb(NMI_DISABLE | REG_B, RTC_REG_PORT);
+    curr = inb(RW_CMOS_PORT);
+    outb(NMI_DISABLE | REG_B, RTC_REG_PORT);
+    outb(curr & 0xBF, RW_CMOS_PORT);  /* turn off bit 6 of register B */
+    sti();
+
+    return 0;
 }
