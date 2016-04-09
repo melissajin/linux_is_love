eaa008cb6b8f36a13d339eb97891f81c93320640
unknown <melissa.jin.11@gmail.com>
2016-04-09 11:31:00 -0500

    Cherry pick fops_t definition

---
+++ student-distrib/process.h
@@ -0,0 +1,22 @@
+
+#ifndef _PROCESS_H
+#define _PROCESS_H
+
+#include "fs.h"
+
+#define FILE_ARRAY_LEN 8
+
+typedef struct pcb {
+	fd_t files[FILE_ARRAY_LEN];
+	int32_t pid;
+	int32_t parent_pid;
+} pcb_t;
+
+typedef struct {
+    fops_t * fops;
+    inode_t * inode;
+    uint32_t pos
+    uint32_t flags;
+} fd_t;
+
+#endif /* _PROCESS_H */
