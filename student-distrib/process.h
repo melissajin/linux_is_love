19dbb8a297d8043ca9f25e37346672f660a0e19b
Joshua Zeid <jrzeid23@gmail.com>
2016-04-10 17:47:14 -0500

    Add device fops table

--- student-distrib/process.h
+++ student-distrib/process.h
@@ -6,17 +6,20 @@
 
 #define FILE_ARRAY_LEN 8
 
+typedef struct {
+    fops_t * fops;
+    inode_t * inode;
+    uint32_t pos;
+    uint32_t flags;
+} fd_t;
+
 typedef struct pcb {
 	fd_t files[FILE_ARRAY_LEN];
 	int32_t pid;
 	int32_t parent_pid;
 } pcb_t;
 
-typedef struct {
-    fops_t * fops;
-    inode_t * inode;
-    uint32_t pos
-    uint32_t flags;
-} fd_t;
+int add_device(char * name, fops_t * fops);
+fops_t * get_device_fops(char * req_name);
 
 #endif /* _PROCESS_H */
